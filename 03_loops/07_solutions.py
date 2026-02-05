6913eecc85a7782f70afd81f173d5670958607f1
prakhar17072004 <pmankur2004@gmail.com>
2026-02-05 17:00:25 +0530

    take user input

---
+++ 03_loops/07_solutions.py
@@ -0,0 +1,10 @@
+# take input from user till 1 to 10
+
+while True:
+    num = int(input("Enter the vlaue between 1 to 10 :"))
+    if 1<= num <=10:
+        print("Thanks")
+        break
+    else:
+        print("Invalid input,try again")
+
