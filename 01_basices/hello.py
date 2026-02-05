140742bf8d95e8322e1124d0a1ebecbcdbe44df7
prakhar17072004 <pmankur2004@gmail.com>
2026-02-05 11:42:49 +0530

    first commit

---
+++ 01_basices/hello.py
@@ -0,0 +1,6 @@
+#print("Hello prakhar")
+
+def prakhar(n):
+    print(n)
+
+prakhar("hello prakhar")    
---
+++ 02_conditianls/01_slolutions.py
@@ -0,0 +1,10 @@
+user_age = int(input("Provide the age "))
+if user_age<13:
+    print("child")
+elif user_age < 20 :
+    print("teenager")
+elif user_age <60 :
+    print("adult")
+else:
+    print("senior citizen")
+
