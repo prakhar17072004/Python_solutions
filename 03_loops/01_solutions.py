71da1d46bcfb3ac2ae5112d90a3632ee36d494be
prakhar17072004 <pmankur2004@gmail.com>
2026-02-05 15:46:43 +0530

    lloops program is added

---
+++ 03_loops/01_solutions.py
@@ -0,0 +1,9 @@
+numbers =[1,-3,4,-5,6,8,-9]
+positive_numbers_count=0;
+for num in numbers:
+    if num >0:
+        # print(num)
+        positive_numbers_count+=1
+print("Total positive number is :",positive_numbers_count)        
+
+    
