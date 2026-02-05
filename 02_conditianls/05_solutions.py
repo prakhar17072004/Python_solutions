2cee820d0ee513cb5a2f5c65cece0df371c8637e
prakhar17072004 <pmankur2004@gmail.com>
2026-02-05 12:49:34 +0530

    weather contion progrma is added

---
+++ 02_conditianls/05_solutions.py
@@ -0,0 +1,10 @@
+weather = input("Provoid weather :").lower()
+
+if weather == "sunny":
+    activity ="Go for a walk"
+elif weather == "rainy":
+    activity = "Read a book "
+elif weather == "snowy":
+    activity = "Buid a snowman"
+
+print(activity)            
