b6a3017835281a8fc252ca84d8a9a6d6c5b70a3c
prakhar17072004 <pmankur2004@gmail.com>
2026-02-05 13:11:32 +0530

    transprot program is added

--- 02_conditianls/06_solutions.py
+++ 02_conditianls/06_solutions.py
@@ -2,9 +2,9 @@ distance = int(input("Provoide distance :"))
  
 if distance < 3:
     transportation = "walk"
-elif distance <15:
+elif distance <=15:
     transportation="bike"
 else:
     transportation="car"      
  
-print(transportation)   
+print("The best transprot medium according to distance is ",transportation)   
