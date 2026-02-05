2859e992c997494db6725e44f1add0e8046fe4a8
prakhar17072004 <pmankur2004@gmail.com>
2026-02-05 15:56:28 +0530

    revesed string program is added

--- 03_loops/03_solutions.py
+++ 03_loops/03_solutions.py
@@ -1,6 +1,6 @@
 n= int(input("Enter the number :"))
 
 for i in range(1,11):
-    if i==5:
-        continue
+    # if i==5:
+    #     continue
     print(i," * ",n," = ",i*n)
---
+++ 03_loops/04_solutions.py
@@ -0,0 +1,9 @@
+# Reverse string program
+
+input_str = input("Provide the string :")
+reversed_str = ""
+
+for char in input_str:
+    reversed_str= char+reversed_str;
+print(reversed_str)
+
