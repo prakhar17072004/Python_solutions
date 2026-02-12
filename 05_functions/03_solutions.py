f7d649ebce9a908983443d05fdceab3fddfd69b0
prakhar17072004 <pmankur2004@gmail.com>
2026-02-12 23:29:34 +0530

    polymorpism program is added

--- 05_functions/03_solutions.py
+++ 05_functions/03_solutions.py
@@ -1,8 +1,9 @@
 def multiply(p1,p2):
     return p1*p2
 
-print(multiply(8,6))
-print(multiply('a',6))
-print(multiply(5,'y'))
+# print(multiply(8,6))
+# print(multiply('a',6))
+# print(multiply(5,'y'))
  
-# res = multiply()
+res = multiply(8,9)
+print(res)
