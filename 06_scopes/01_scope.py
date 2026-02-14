50dd976d1a132a2605262bee22f54e2fef09d874
prakhar17072004 <pmankur2004@gmail.com>
2026-02-14 14:30:02 +0530

    scope funtion added

--- 06_scopes/01_scope.py
+++ 06_scopes/01_scope.py
@@ -5,3 +5,18 @@ def func():
     print(username)
 print(username)    
 func()
+
+x=99
+
+# def func2(y):
+#     z=x+y
+#     return z
+
+# res = func2(1)
+# print(res)
+
+# def fun3():
+#     x=88
+
+# fun3()
+# print(x)    
