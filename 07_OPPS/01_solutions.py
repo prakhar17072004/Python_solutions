e6dc3a6068b00e90ee00d681091fae6d74a2cd8f
prakhar17072004 <pmankur2004@gmail.com>
2026-02-16 15:29:47 +0530

    scope is added

--- 07_OPPS/01_solutions.py
+++ 07_OPPS/01_solutions.py
@@ -1,5 +1,14 @@
 class Car:
-    brand=None
-    model= None
+    # self is here like this keyword
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model= model
+    def full_name(self):
+        return f"{self.brand} {self.model}"
 
-my_car = Car()    
+      
+
+my_car = Car("Toyata","Corolla") 
+print(my_car.brand)
+print(my_car.model)
+print(my_car.full_name())
