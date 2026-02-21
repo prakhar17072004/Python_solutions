48affba016153863d45886f589fdb726e3212605
prakhar17072004 <pmankur2004@gmail.com>
2026-02-21 17:26:53 +0530

    updelete logic add using database mechanism

--- 10_database_sqlite3/youtube_manager_db.py
+++ 10_database_sqlite3/youtube_manager_db.py
@@ -21,6 +21,9 @@ def add_videos(videos):
 def update_videos(videos):
     pass
 
+def delete_videos(videos):
+    pass
+
 
 
 def main():
@@ -44,6 +47,11 @@ def main():
             name=input("Enter the videos name :")
             time= input("Enter the video time")
             update_videos(videos_id,name,time)
+
+        elif choice == '4':
+            videos_id=input("Enter the video id for update :")
+            
+            delete_videos(videos_id)    
 
 
 
